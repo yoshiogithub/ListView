5b2bbce652fbf86b113fce26adac8633b9e43c12
Yoshio Hiromitsu <yox@MacBook-Pro-de-Yoshio.local>
2020-06-05 22:49:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView=findViewById(R.id.listView);
+
+        ArrayAdapter<CharSequence>adaptador=ArrayAdapter.createFromResource(this,R.array.arrayDias,android.R.layout.simple_spinner_item);
+        listView.setAdapter(adaptador);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(), "Seleccionado : "+parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
